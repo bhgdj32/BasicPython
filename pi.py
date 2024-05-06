df7476d4d67dc3fefd804ac6e8a7820917c7c6fe
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2024-05-06 20:17:56 +0900

    pythonやり直し

--- pi.py
+++ pi.py
@@ -1,6 +1,7 @@
 s = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
 m = s.replace(",","")
-a = m.split()
+n = m.replace(".","")
+a = n.split()
 p = list(map(len,a))
 k = ''.join(map(str,p))
 print(k)
