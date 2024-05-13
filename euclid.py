7e34d0f81f407b3fc6b108beb3de043adf18b4a5
junfgdj <149660238+bhgdj32@users.noreply.github.com>
2024-05-13 12:34:49 +0900

    関数やり直し3

--- euclid.py
+++ euclid.py
@@ -1,11 +1,11 @@
+a = input()
+b = input()
 def euclid(a,b):
     r = 1
     while r != 0:
         r = a % b
         a , b = b , r
-return a
-a = int(input())
-b = int(input())
+    return a
 def euclid_prime_number(a,b):
     if euclid(a , b) == 1:
         return True
